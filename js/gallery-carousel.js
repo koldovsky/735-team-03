ed9ff0f87823ac3517d1e939647ccd1ce1c29539
Hryhorii <81336513+grischat@users.noreply.github.com>
2023-02-05 17:26:20 +0000

    Carousel push

--- js/gallery-carousel.js
+++ js/gallery-carousel.js
@@ -1,7 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.gallery__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
         arrows: true,
-        dots: true,
-        adaptiveHeight: true,
+        fade: true,
+        asNavFor: '.slider__nav'
     });
+    $('.slider__nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 0,
+        asNavFor: '.gallery__slider',
+        centerMode: true,
+        focusOnSelect: true
+    })
 });
