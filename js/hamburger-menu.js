475c3d5be7f3fd627059e8fc6b81915700b283cb
bwaah1 <ivan.chyk.ik.2021@lpnu.ua>
2023-02-06 22:32:45 +0200

    Add IIFE to hamburger-menu.js

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,33 +1,35 @@
-const hamb = document.querySelector("#hamb");
-const popup = document.querySelector("#popup");
-const body = document.body;
+(function () {
+  const hamb = document.querySelector("#hamb");
+  const popup = document.querySelector("#popup");
+  const body = document.body;
 
-const menu = document.querySelector("#menu").cloneNode(1);
-const line = document.querySelector("#line").cloneNode(1);
+  const menu = document.querySelector("#menu").cloneNode(1);
+  const line = document.querySelector("#line").cloneNode(1);
 
-hamb.addEventListener("click", hambHandler);
+  hamb.addEventListener("click", hambHandler);
 
-function hambHandler(e) {
-  e.preventDefault();
-  popup.classList.toggle("open");
-  hamb.classList.toggle("active");
-  body.classList.toggle("noscroll");
-  renderPopup();
-}
+      function hambHandler(e) {
+      e.preventDefault();
+      popup.classList.toggle("open");
+      hamb.classList.toggle("active");
+      body.classList.toggle("noscroll");
+      renderPopup();
+    }
 
-function renderPopup() {
-  popup.appendChild(menu);
-  popup.appendChild(line);
-}
+  function renderPopup() {
+    popup.appendChild(menu);
+    popup.appendChild(line);
+  }
 
-const links = Array.from(menu.children);
+  const links = Array.from(menu.children);
 
-links.forEach((link) => {
-  link.addEventListener("click", closeOnClick);
-});
+  links.forEach((link) => {
+    link.addEventListener("click", closeOnClick);
+  });
 
-function closeOnClick() {
-  popup.classList.remove("open");
-  hamb.classList.remove("active");
-  body.classList.remove("noscroll");
-}
+  function closeOnClick() {
+    popup.classList.remove("open");
+    hamb.classList.remove("active");
+    body.classList.remove("noscroll");
+  }
+})();
